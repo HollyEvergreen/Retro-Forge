1bae559c2caff1c543105deb50aaafd742ba4401
HollyEvergreen <57192706+HollyEvergreen@users.noreply.github.com>
2025-12-03 14:37:02 +0000

    all tests passed

---
+++ src/RenderForge.cs
@@ -0,0 +1,24 @@
+﻿using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+
+public class RenderForge
+{
+    private GameWindow gameWindow;
+    
+    
+    
+    public void RegisterWindow(ref GameWindow gameWindow)
+    {
+        this.gameWindow = gameWindow;
+    }
+
+    public void Render(FrameEventArgs frameEventArgs)
+    {
+        
+    }
+    
+    public void Update(FrameEventArgs frameEventArgs)
+    {
+        
+    }
+}
--- src/program.cs
+++ src/program.cs
@@ -1,41 +1,48 @@
-﻿using System.Numerics;
+﻿using System.Diagnostics;
 using OpenTK.Mathematics;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
-using number = double;
+using Number = double;
 
-namespace Retro_Forge;
-internal class Program
+var settings = new GameWindowSettings()
 {
-    public static void _Main(string[] args)
-    {
-        var settings = new GameWindowSettings()
-        {
-            UpdateFrequency = 144,
-            Win32SuspendTimerOnDrag = false
-        };
-        var nativeWindowSettings = new NativeWindowSettings()
-        {
-            AlphaBits = 8,
-            API = ContextAPI.OpenGL,
-            APIVersion = new Version(4, 6, 0),
-            AspectRatio = (16, 9),
-            AutoIconify = false,
-            AutoLoadBindings = true,
-            RedBits = 8,
-            BlueBits = 8,
-            GreenBits = 8,
-            DepthBits = 16,
-            ClientSize = new Vector2i(1920, 1080),
-            Flags = ContextFlags.ForwardCompatible,
-            Title = "RetroForge",
-            SrgbCapable = true,
-            Vsync = VSyncMode.Adaptive,
-        };
-        
-        
-        
-        var gameWindow = new GameWindow(settings, nativeWindowSettings);
-        return;
-    }
-}
+    UpdateFrequency = 144,
+    Win32SuspendTimerOnDrag = false
+};
+var nativeWindowSettings = new NativeWindowSettings()
+{
+    AlphaBits = 8,
+    API = ContextAPI.OpenGL,
+    APIVersion = new Version(4, 6, 0),
+    AspectRatio = (16, 9),
+    AutoIconify = false,
+    AutoLoadBindings = true,
+    RedBits = 8,
+    BlueBits = 8,
+    GreenBits = 8,
+    DepthBits = 16,
+    ClientSize = new Vector2i(1920, 1080),
+    Flags = ContextFlags.ForwardCompatible,
+    Title = "RetroForge",
+    SrgbCapable = true,
+    Vsync = VSyncMode.Adaptive,
+};
+
+
+const Number dt = 0;
+Console.WriteLine(dt);
+var gameWindow = new GameWindow(settings, nativeWindowSettings);
+var renderForge = new RenderForge();
+
+
+Debug.Assert(gameWindow != null, nameof(gameWindow) + " != null");
+Debug.Assert(renderForge != null, nameof(RenderForge) + " != null");
+
+renderForge.RegisterWindow(ref gameWindow);
+
+gameWindow.Load += () => { };
+gameWindow.RenderFrame += renderForge.Render;
+gameWindow.UpdateFrame += renderForge.Update;
+
+
+gameWindow.Close();
